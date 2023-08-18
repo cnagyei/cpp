0542b35fbaacdf37695c5491edd9ddb0529fd869
cnagyei <cnagyei15@gmail.com>
2023-08-17 18:49:12 -0700

    add files

---
+++ 2-cout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello World!\n";
+	cout << "I'm learning c++\n";
+
+	return (0);
+}
---
+++ 3-new_line.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello World!" << endl;	// endl - breaks line
+	cout << "I'm learning c++\n";	// \n - new line
+
+	return (0);
+}
